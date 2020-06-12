f9218f5b4fbc4e3446443901ace9e981b8874560
shawavisek35 <shawavisek35@gmail.com>
2020-06-12 19:39:56 +0530

    Starting frontend

--- backend/routes/exercise.js
+++ backend/routes/exercise.js
@@ -27,4 +27,28 @@ router.route("/add").post((req,res) => {
     .catch(err => res.status(400).json("Error : " + err));
 });
 
+router.route("/:id").get((req,res) => {
+    Exercise.findById(req.params.id)
+    .then(exercise => res.json(exercise))
+    .catch(err => res.status(400).json("Error : " + err));
+});
+
+router.route("/:id").delete((req,res)=>{
+    Exercise.findByIdAndDelete(req.params.id)
+    .then(id => res.json(id + "Successfully deleted...."))
+    .catch(err => res.status(400).json("Error : " + err));
+});
+
+router.route("/update/:id").post((req,res)=> {
+    Exercise.findByIdAndUpdate(
+        {_id : req.params.id},
+        {
+            username : req.body.username,
+            description : req.body.description
+        }
+    )
+    .then(() => res.json("Successfully Updated....."))
+    .catch(err => res.status(400).json("Error : " + err));
+});
+
 module.exports = router;
